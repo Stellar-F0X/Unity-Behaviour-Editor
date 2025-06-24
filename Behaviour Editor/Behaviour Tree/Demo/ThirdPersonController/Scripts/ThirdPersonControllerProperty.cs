8c3c0dd517d6ead26e613fe4872cba6b0a5fbc88
Stellar-F0X <jms030907@gmail.com>
2025-06-24 18:58:56 +0900

    chore: 데모를 유니티 패키지로 수정

--- Behaviour Editor/Behaviour Tree/Demo/ThirdPersonController/Scripts/ThirdPersonControllerProperty.cs
+++
@@ -1,9 +0,0 @@
-using StarterAssets;
-using UnityEngine;
-
-namespace BehaviourSystem.BT.Demo
-{
-    public class ThirdPersonControllerProperty : BlackboardProperty<ThirdPersonController> { }
-
-    public class TransformListProperty : GenericListProperty<Transform> { }
-}
